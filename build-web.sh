02b8a3f0d495fd20b48c2ea9cd003421fbddf9a7
learnforpractice <learnforpractice@gmail.com>
2021-05-19 21:43:20 +0800

    Update build-web.sh

--- build-web.sh
+++ build-web.sh
@@ -1,3 +1,4 @@
 yarn build-web
 cp dist-web/wallet.js /Users/newworld/dev/mixin/defis/static/js/wallet.js
+cp dist-web/wallet.min.js /Users/newworld/dev/mixin/defis/static/js/wallet.min.js
 cp dist-web/wallet.js /Users/newworld/dev/react/scatter-demo-eosjs2/src/components
