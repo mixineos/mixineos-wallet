84e70759dc5a0f23bf6d39e013a78433c5391960
learnforpractice <learnforpractice@gmail.com>
2021-05-26 07:59:06 +0800

    1.Fix generateCreateAccountTx 2. Add generateBindAccountTx

--- src/tx_generator.ts
+++ src/tx_generator.ts
@@ -35,7 +35,7 @@ const generateDepositTx = async(api: Api, account: string, amount: string, token
     );
     // console.log("++++transaction:", transaction);
     const trx = api.deserializeTransaction(transaction.serializedTransaction);
-    // console.log("++++trx:", trx);
+    console.log("++++deposit trx:", JSON.stringify(trx));
     return [trx, transaction];
 }
 
@@ -90,7 +90,7 @@ const generateWithdrawTx = async(api: Api, account: string, amount: string, toke
 
 const generateCreateAccountTx = async(api: Api, user_id: string, new_account: string, amount: string) => {
     const _user_id = '0x' + replaceAll(user_id, "-", "");
-    const str_amount = parseFloat(amount).toFixed(8);
+    const str_amount = parseFloat(amount).toFixed(4);
 
     let transaction = await api.transact(
         {
@@ -121,8 +121,108 @@ const generateCreateAccountTx = async(api: Api, user_id: string, new_account: st
     );
     // console.log("++++transaction:", transaction);
     const trx = api.deserializeTransaction(transaction.serializedTransaction);
+    console.log("++++trx:", trx);
+    return [trx, transaction];
+}
+
+const generateBindAccountTx = async(api: Api, user_id: string, account: string) => {
+    const info = await api.rpc.get_account(account);
+    const active_perm = info.permissions.find((x: any) => x.perm_name == 'active');
+
+    if (active_perm.required_auth.keys.length !== 1) {
+        throw Error("unsupported account permission!");
+    }
+
+    if (active_perm.required_auth.accounts.length !== 0) {
+        throw Error("unsupported account permission!");
+    }
+    
+    if (active_perm.required_auth.threshold !== 1) {
+        throw Error("unsupported account permission!");
+    }
+
+    const pub_key = active_perm.required_auth.keys[0].key
+
+    active_perm.required_auth.accounts = [
+        {
+            permission: {
+                actor: 'mixincrossss',
+                permission: 'multisig'
+            },
+            weight: 1
+        }
+    ];
+
+    const auth = {
+        account: account,
+        permission: "active",
+        parent: "owner",
+        auth: active_perm.required_auth
+        // auth: {
+        //     threshold: 1,
+        //     keys: [
+        //         {
+        //             key: "EOS7t7CbbYcu3mMjDnWmg4meCNqTbNfUpdWKxNErMFWMcyuzdyzSB",
+        //             weight: 1
+        //         },
+        //     ],
+        //     accounts: [] as any[],
+        //     waits: [] as any[]
+        // }
+    }    
+
+    const _user_id = '0x' + replaceAll(user_id, "-", "");
+
+    let transaction = await api.transact(
+        {
+          actions: [
+            {
+                account: 'eosio',
+                name: "updateauth",
+                authorization: [
+                    {
+                        actor: account,
+                        permission: "active"
+                    }
+                ],
+                data: auth
+            },
+            {
+                account: MAIN_CONTRACT,
+                name: "bindacc",
+                authorization: [
+                    {
+                        actor: MAIN_CONTRACT,
+                        permission: "active"
+                    },
+                    {
+                        actor: account,
+                        permission: "active"
+                    }
+                ],
+                data: {
+                    account: account,
+                    user_id: _user_id,
+                }
+            }
+          ]
+        },
+        {
+            broadcast: false,
+            sign: false,
+            blocksBehind: 3,
+            expireSeconds: 60*60
+        }
+    );
+    // console.log("++++transaction:", transaction);
+    const trx = api.deserializeTransaction(transaction.serializedTransaction);
     // console.log("++++trx:", trx);
     return [trx, transaction];
 }
 
-export { generateDepositTx, generateWithdrawTx, generateCreateAccountTx }
+export {
+    generateDepositTx,
+    generateWithdrawTx,
+    generateCreateAccountTx,
+    generateBindAccountTx
+}
